0a4b8f399f66e343bca09f41273847403a011e8a
Aliaksei Kazlou <kozlovalex2005@gmail.com>
2026-02-12 14:12:10 +0100

    Update branding and lock to portrait orientation
    - Update splash screen logo from 'Puzzle Dazzle' to 'Mazele Dazzle'
    - Lock app to portrait orientation on Android for better maze display
    - Portrait mode provides more vertical space for maze rendering
    
    Changes ensure consistent branding and optimal user experience for
    maze viewing and interaction.

--- src/PuzzleDazzle/Platforms/Android/MainActivity.cs
+++ src/PuzzleDazzle/Platforms/Android/MainActivity.cs
@@ -1,10 +1,10 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
 namespace PuzzleDazzle;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
